fc3baa9135dbc65797504dcad92400302ac72b75
Jonas Dietrich <joaldi.98@gmail.com>
2023-05-06 21:22:27 +0200

    installation requirements

---
+++ init.bash
@@ -0,0 +1,7 @@
+## build setup for SSP usage
+# build softlink to parent directory (where topspin looks for scripts)
+ln -s ../ssp.py ssp.py
+# move to parent directory
+cd ..
+# create a new conda environment (has to be set in topspin as python 3 env)
+conda create --name sec-struc-predictor --file SSP/requirements.txt
