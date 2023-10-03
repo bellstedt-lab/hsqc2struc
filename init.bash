bd6df255fa13f91bc5602f3237b935ed248fe562
Peter Bellstedt <37342624+bellstedt-lab@users.noreply.github.com>
2023-10-03 17:08:12 +0200

    Delete init.bash

--- init.bash
+++
@@ -1,7 +0,0 @@
-## build setup for SSP usage
-# build softlink to parent directory (where topspin looks for scripts)
-ln -s ../ssp.py ssp.py
-# move to parent directory
-cd ..
-# create a new conda environment (has to be set in topspin as python 3 env)
-conda create --name hsqc2struc --file hsqc2struc/requirements.txt
