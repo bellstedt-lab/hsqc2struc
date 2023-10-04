081786b9494670524698af2a9955516e5e4b2fdc
Peter Bellstedt <37342624+bellstedt-lab@users.noreply.github.com>
2023-10-04 14:53:31 +0200

    Update install_ssp.bash

--- install_ssp.bash
+++ install_ssp.bash
@@ -15,14 +15,15 @@ base_dir=$(dirname $(dirname $conda_path))
 # Run initialization script
 source "$base_dir/etc/profile.d/conda.sh"
 
-cd ..
 conda create --name hsqc2struc python=3.10
 conda activate hsqc2struc
 conda install pip
 
-python3 -m pip install hsqc2struc/requirements.txt ts_remote_api*.whl bruker_nmr_api*.whl
-ln -s hsqc2struc/ssp.py ssp.py
+python3 -m pip install --upgrade requirements.txt ../ts_remote_api*.whl ../bruker_nmr_api*.whl --no-input
+ln -s ssp.py ../ssp.py
 
 python_path=$(which python)
+
+echo "Installation (hopefully) finished"
 echo "Your Hsqc2Struc Python executable is located at: $python_path"
-echo "Installation finished. Please open TopSpin, enter 'set' in the commandline, navigate to Python3 Environment and paste the above path. Further Information can be found in the Readme.md file."
+echo " Please open TopSpin, enter 'set' in the commandline, navigate to Python3 Environment and paste the above path. Further Information can be found in the Readme.md file."
